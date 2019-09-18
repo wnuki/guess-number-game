4d8b83354bb96a38ec604fd2ad7b8155e90b2308
Kinga Wnuk <kinga.wnuk@protonmail.com>
2019-09-18 09:58:13 +0200

    use Spring container

--- core/src/main/java/com/java/Main.java
+++ core/src/main/java/com/java/Main.java
@@ -2,11 +2,25 @@ package com.java;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     private static final Logger log = LoggerFactory.getLogger(Main.class);
+    private static final String CONFIG_LOCATION = "beans.xml";
 
     public static void main(String[] args) {
         log.info("Guess the number game");
+
+        ConfigurableApplicationContext context
+                = new ClassPathXmlApplicationContext(CONFIG_LOCATION);
+
+        NumberGenerator numberGenerator
+                = context.getBean("numberGenerator", NumberGenerator.class);
+
+        int number = numberGenerator.next();
+        log.info("number: {}", number);
+
+        context.close();
     }
 }
