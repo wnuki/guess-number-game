d7916860bf5c585762853f091c61c4a87bdc5fb4
Kinga Wnuk <kinga.wnuk@protonmail.com>
2019-09-19 00:11:56 +0200

    add game.properties file

--- core/src/main/java/com/java/config/GameConfig.java
+++ core/src/main/java/com/java/config/GameConfig.java
@@ -2,13 +2,20 @@ package com.java.config;
 
 import com.java.GuessCount;
 import com.java.MaxNumber;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@PropertySource("classpath:config/game.properties")
 public class GameConfig {
-    private int maxNumber = 50;
-    private int guessCount = 5;
+
+    @Value("${game.maxNumber:100}")
+    private int maxNumber;
+
+    @Value("${game.guessCount:10}")
+    private int guessCount;
 
     @Bean
     @MaxNumber
