83e2232809c675b079a5615ab9dda8f332536866
Kinga Wnuk <kinga.wnuk@protonmail.com>
2019-09-18 10:26:25 +0200

    implement the game

---
+++ core/src/main/java/com/java/Game.java
@@ -0,0 +1,25 @@
+package com.java;
+
+public interface Game {
+    int getNumber();
+
+    int getGuess();
+
+    void setGuess(int guess);
+
+    int getSmallest();
+
+    int getBiggest();
+
+    int getRemainingGuesses();
+
+    void reset();
+
+    void check();
+
+    boolean isValidNumberRange();
+
+    boolean isGameWon();
+
+    boolean isGameLost();
+}
