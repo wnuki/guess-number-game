50b98f081ec4bbd336ef3594ad5425214a386e75
Kinga Wnuk <kinga.wnuk@protonmail.com>
2019-09-18 22:46:14 +0200

    setup the console module

--- console/src/main/java/com/java/console/Main.java
+++ console/src/main/java/com/java/console/Main.java
@@ -1,5 +1,8 @@
-package com.java;
+package com.java.console;
 
+import com.java.AppConfig;
+import com.java.MessageGenerator;
+import com.java.NumberGenerator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ConfigurableApplicationContext;
